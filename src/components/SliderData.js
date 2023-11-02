cc8788e2a6051890ab963d563c2a0269f2010f67
ekemmstar <103825682+ekemmstar@users.noreply.github.com>
2023-11-02 20:45:41 +0200

    Image Slider

---
+++ src/components/SliderData.js
@@ -0,0 +1,26 @@
+export const SliderData = [
+{
+    image:
+    'https://images.unsplash.com/photo-1683009427037-c5afc2b8134d?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2fHx8ZW58MHx8fHx8'    
+
+},
+{
+    image: 
+    'https://images.unsplash.com/photo-1698612352839-b4987b606084?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fHx8'
+
+},
+{
+    image: 
+    'https://images.unsplash.com/photo-1698840473362-5a73b1fa7eaf?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8fA%3D%3D'
+
+},
+{
+    image:
+    'https://images.unsplash.com/photo-1698793916523-a7db028b148b?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyOXx8fGVufDB8fHx8fA%3D%3D'
+},
+{
+    image:
+    'https://images.unsplash.com/photo-1682685797406-97f364419b4a?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw0MXx8fGVufDB8fHx8fA%3D%3D'
+}
+]
+   
